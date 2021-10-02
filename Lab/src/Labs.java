08636044e3e0c3a78aa6c8d46f38b2b0ccb66521
Arie Hendrix <naalix@yahoo.com>
2021-10-02 12:00:48 -0700

    Testing a new upload. This stuff is really confusing.

---
+++ Lab/src/Labs.java
@@ -0,0 +1,27 @@
+
+public class Labs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int x = 5;
+//		if (x > 0){
+//	        for (int i = 0; i <= x; i++){
+//	           System.out.println(i);
+//	         }
+//	        }else{
+//	       //Use this line for printing to the console if the value of x is less than 1.
+//	       System.out.println("Please enter a number greater than 0.");
+//	        }
+		for (int i = 1; i < 100; i+=2){
+		     System.out.println(i);
+		   }
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
